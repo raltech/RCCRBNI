46dfa506dd2030275f5e2e9b75f67ac9656ea788
raltech <raltech.dev@gmail.com>
2023-03-17 16:24:05 -0700

    ucb1

--- agent/ucb1_agent.py
+++ agent/ucb1_agent.py
@@ -1,36 +1,30 @@
 import numpy as np
-import math
-from base_agent import Agent
+from tqdm import tqdm
 
-class UCB1Agent(Agent):
-    def __init__(self, action_space, learning_rate=0.01, discount_factor=0.99):
-        super().__init__(action_space, learning_rate, discount_factor)
-        self.n_actions = len(action_space)
-        self.action_counts = np.zeros(self.n_actions)
-        self.action_values = np.zeros(self.n_actions)
+# UCB1 Agent
+class UCB1Agent:
+    def __init__(self, env, gamma=0.9, c=0.5, lr=0.1):
+        self.env = env
+        self.gamma = gamma
+        self.c = c
+        self.lr = lr
+        self.Q = np.zeros((env.n_states, env.n_elecs*env.n_amps), dtype=np.float32)
+        self.n = np.zeros((env.n_states, env.n_elecs*env.n_amps), dtype=np.uint16)
+        self.done = False
+    
+    def choose_action(self):
+        sum_n = np.sum(self.n[self.env.state])
+        bonus = self.c*np.sqrt(np.log(sum_n)/self.n[self.env.state])
 
-    def choose_action(self, _=None):
-        total_counts = np.sum(self.action_counts)
+        action_idx = np.argmax(self.Q[self.env.state]+bonus)
+        return action_idx
+    
+    def update(self, state, action, reward, next_state):
+        self.n[state, action] += 1
+        self.Q[state, action] += self.lr*(reward + self.gamma*np.max(self.Q[next_state]) - self.Q[state, action])
 
-        if total_counts == 0:
-            # Choose a random action if no action has been taken yet
-            return np.random.choice(self.n_actions)
-
-        # Calculate UCB-1 values for all actions
-        ucb_values = self.action_values + np.sqrt(2 * math.log(total_counts) / self.action_counts)
-
-        # Choose the action with the highest UCB-1 value
-        return np.argmax(ucb_values)
-
-    def update(self, _, action, reward, __, ___):
-        # Update the action counts and values
-        self.action_counts[action] += 1
-        self.action_values[action] += (reward - self.action_values[action]) / self.action_counts[action]
-
-    def save(self, file_path):
-        # Saving and loading not implemented for this example
-        pass
-
-    def load(self, file_path):
-        # Saving and loading not implemented for this example
-        pass
+    def get_Q(self):
+        return self.Q.copy()
+    
+    def get_n(self):
+        return self.n.copy()
