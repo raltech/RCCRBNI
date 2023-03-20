5c70a1770f9c8bfbf56ebec9ca873c49159dc50c
raltech <raltech.dev@gmail.com>
2023-03-20 02:39:08 -0700

    run all experiments

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 import numpy as np
 from env.single_state_env import SingleStateEnvironment
-from utils.reward_func import inverse_reward_func, scatter_reward_function, diversity_reward_function, more_cell_reward_func
+from utils.reward_func import inverse_reward_func, diversity_reward_function, more_cell_reward_func
 from utils.score_func import span_score_func, scatter_matrix_score_func, qr_rank_score_func, RREF_score_func, relevance_score_func
 from utils.helper import load_dictionary, action2elec_amp
 from agent.epsilon_greedy import EpsilonGreedyAgent
@@ -16,21 +16,19 @@ N_ELECTRODES = 512
 N_AMPLITUDES = 42
 N_EXAUSTIVE_SEARCH = N_ELECTRODES * N_AMPLITUDES * 25
 
-def main(n_search, log_freq):
-    score_func = relevance_score_func
-    reward_func = diversity_reward_function
-    experiments = ["2022-11-04-2", "2022-11-28-1"]
-    experiment = experiments[0]
+def main(n_search, log_freq, experiment, score_func, reward_func, n_avg_itr):
+    score_func = score_func
+    reward_func = reward_func
+    experiment = experiment
+    n_avg_itr = n_avg_itr
     path = f"./data/{experiment}/dictionary"
     usage_path = f"/Volumes/Scratch/Users/ajphillips/gdm_streamed/{experiment}/estim" # TODO CHANGEME
     usage_path = f"./data/{experiment}/estim"
     agent_list = ["RandomAgent", "EpsilonGreedyAgent", "DecayEpsilonGreedyAgent", 
                   "UCB1Agent", "SARSAAgent", "SARSAUCB1Agent"]
-    # agent_list = ["UCB1Agent"]
 
     # data: (dict, elecs, amps, elec_map, cell_ids, usage)
     data = load_dictionary(path, usage_path)
-    # import pdb; pdb.set_trace()
 
     # calculate the span score of the dictionary
     baseline_score = score_func(data[0], dict_hat_count=np.ones(N_ELECTRODES*N_AMPLITUDES)*25, relevance=data[-1])
@@ -38,7 +36,6 @@ def main(n_search, log_freq):
     
     # average the scores for multiple runs
     avg_score_hist_list = []
-    n_avg_itr = 10
     
     for agent_name in agent_list:
         print("\n========================================")
@@ -53,7 +50,8 @@ def main(n_search, log_freq):
             # initialize the environment
             env = SingleStateEnvironment(data, reward_func=reward_func, n_maxstep=N_EXAUSTIVE_SEARCH, 
                                          n_elecs=N_ELECTRODES, n_amps=N_AMPLITUDES)
-            
+            if agent_name == "RandomAgent" and avg_itr == 1:
+                break
             # initialize the agent
             if agent_name == "RandomAgent":
                 agent = EpsilonGreedyAgent(env, gamma=0.9, epsilon=1.0, decay_rate=1, lr=0.1)
@@ -84,12 +82,18 @@ def main(n_search, log_freq):
                         score_hist.append(0)
                     else:
                         score_hist.append(score_func(approx_dict, dict_hat_count=env.dict_hat_count, relevance=data[-1]))
+                    if score_hist[-1] > baseline_score*2:
+                        print("\nFound a 2x better dictionary at step {}. Score: {}".format(i, score_hist[-1]))
+                        # append the score_hist with the same score for the rest of the search
+                        # import pdb; pdb.set_trace()
+                        score_hist.extend([score_hist[-1]]*((n_search-i)//log_freq))
+                        break
                 action = agent.choose_action()
                 next_state, reward, done = env.step(action2elec_amp(action, N_AMPLITUDES))
                 agent.update(state, action, reward, next_state)
 
             score_hist_list.append(score_hist)
-
+        # import pdb; pdb.set_trace()
         avg_score_hist_list.append(np.mean(score_hist_list, axis=0))
 
     # plot the span scores for each agent in the same plot
@@ -110,16 +114,37 @@ def main(n_search, log_freq):
             ax.plot(avg_score_hist_list[i], label="SARSA,UCB1", linewidth=3)
         
     ax.figure.set_size_inches(20, 15)
-    ax.set_xlabel("Episode {0}k".format(log_freq//1000), fontsize=28)
+    ax.set_xlabel("Step {0}k".format(log_freq//1000), fontsize=28)
     ax.set_ylabel("Score", fontsize=28)
     ax.set_title(f"Scores of each agent on {experiment} with {reward_func.__name__}", fontsize=30)
     ax.tick_params(axis='both', which='major', labelsize=25)
     ax.legend(fontsize=25)
+    ax.set_ylim(0, baseline_score*1.7)
     ax.axvline(x=N_EXAUSTIVE_SEARCH/log_freq, color="black", linestyle="--")
     ax.axhline(y=baseline_score, color="black", linestyle="--")
     fig.savefig(f"./assets/scores/scores_{experiment}_{score_func.__name__}_{reward_func.__name__}_{n_search}_{n_avg_itr}.png", dpi=300)
-    plt.show()
+    # plt.show()
 
 if __name__ == "__main__":
-    main(n_search=505001, log_freq=10000)
-    # main(n_search=10001, log_freq=1000)
+    experiments = ["2022-11-04-2", "2022-11-28-1"]
+    # main(n_search=510001, log_freq=10000, experiment=experiments[0], score_func=relevance_score_func, reward_func=diversity_reward_function)
+    n_search = 520001
+    log_freq = 10000
+    n_avg_itr = 10
+    print("Experiment 1: {}_{}_{}".format(experiments[0], relevance_score_func.__name__, diversity_reward_function.__name__))
+    main(n_search=n_search, log_freq=log_freq, experiment=experiments[0], score_func=relevance_score_func, reward_func=diversity_reward_function, n_avg_itr=n_avg_itr)
+
+    print("Experiment 2: {}_{}_{}".format(experiments[1], relevance_score_func.__name__, diversity_reward_function.__name__))
+    main(n_search=n_search, log_freq=log_freq, experiment=experiments[1], score_func=relevance_score_func, reward_func=diversity_reward_function, n_avg_itr=n_avg_itr)
+    
+    print("Experiment 3: {}_{}_{}".format(experiments[0], relevance_score_func.__name__, inverse_reward_func.__name__))
+    main(n_search=n_search, log_freq=log_freq, experiment=experiments[0], score_func=relevance_score_func, reward_func=inverse_reward_func, n_avg_itr=n_avg_itr)
+    
+    print("Experiment 4: {}_{}_{}".format(experiments[1], relevance_score_func.__name__, inverse_reward_func.__name__))
+    main(n_search=n_search, log_freq=log_freq, experiment=experiments[1], score_func=relevance_score_func, reward_func=inverse_reward_func, n_avg_itr=n_avg_itr)
+    
+    print("Experiment 5: {}_{}_{}".format(experiments[0], relevance_score_func.__name__, more_cell_reward_func.__name__))
+    main(n_search=n_search, log_freq=log_freq, experiment=experiments[0], score_func=relevance_score_func, reward_func=more_cell_reward_func, n_avg_itr=n_avg_itr)
+    
+    print("Experiment 6: {}_{}_{}".format(experiments[1], relevance_score_func.__name__, more_cell_reward_func.__name__))
+    main(n_search=n_search, log_freq=log_freq, experiment=experiments[1], score_func=relevance_score_func, reward_func=more_cell_reward_func, n_avg_itr=n_avg_itr)
